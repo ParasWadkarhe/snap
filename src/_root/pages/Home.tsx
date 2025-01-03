d021052b52b99ceaa4370a82e5b06881b2db46a1
ParasWadkarhe <paraswadkar11@gmail.com>
2025-01-03 12:11:42 +0530

    home error

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -3,29 +3,29 @@ import PostCard from "@/components/shared/PostCard";
 import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutations";
 import { Models } from "appwrite";
 
-
 const Home = () => {
-
-  const { data:posts, isPending:isPostLoading,isError:isErrorPosts} = useGetRecentPosts();
+  const { data: posts, isPending: isPostLoading, isError: isErrorPosts } = useGetRecentPosts();
 
   return (
     <div className="flex flex-1 ">
       <div className="home-container">
         <div className="home-posts">
-            <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
-            {isPostLoading && !posts ? (
-              <Loader/>
-            ):(
-              <ul className="flex flex-col flex-1 gap-9 w-full">
-                {posts?.documents.map((post:Models.Document)=>(
-                  <PostCard key={post.id} post={post} />
-                ))}
-              </ul>
-            )}
+          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+          {isPostLoading && !posts ? (
+            <Loader />
+          ) : isErrorPosts ? (
+            <div className="error-message">Failed to load posts. Please try again later.</div>
+          ) : (
+            <ul className="flex flex-col flex-1 gap-9 w-full">
+              {posts?.documents.map((post: Models.Document) => (
+                <PostCard key={post.id} post={post} />
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import { useCallback, useState } from "react";
 import { useDropzone, FileWithPath } from "react-dropzone";
 import { Button } from "../ui/button";
 
@@ -17,6 +17,7 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
       fieldChange(acceptedFiles);
       setFileUrl(URL.createObjectURL(acceptedFiles[0]));
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [file]
   );
   const { getRootProps, getInputProps } = useDropzone({

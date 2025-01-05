193689c975680998f3c117099e0c259f9e783480
ParasWadkarhe <paraswadkar11@gmail.com>
2025-01-05 13:37:06 +0530

    side-img

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -14,7 +14,7 @@ const AuthLayout = () => {
       <section className="flex flex-1 justify-center items-center flex-col py-10">
         <Outlet/>
       </section>
-      <img src="./assets/images/side-img.svg" alt="logo" className="hidden sm:block h-screen w-1/2 object-cover bg-no-repeat" />
+      <img src="./assets/images/side-img.png" alt="logo" className="hidden sm:block h-screen w-1/2 object-cover bg-no-repeat" />
 
       </>
     )}

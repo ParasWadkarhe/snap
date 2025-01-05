e7bc88f23b68530ef465880e0f7b2379e8ee14b1
ParasWadkarhe <paraswadkar11@gmail.com>
2025-01-05 13:41:45 +0530

    side

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet,Navigate } from "react-router-dom"
-// import sideImg from ''
+import sideImg from './assets/images/side-img.png';
 
 const AuthLayout = () => {
 
@@ -14,7 +14,7 @@ const AuthLayout = () => {
       <section className="flex flex-1 justify-center items-center flex-col py-10">
         <Outlet/>
       </section>
-      <img src="./assets/images/side-img.png" alt="logo" className="hidden sm:block h-screen w-1/2 object-cover bg-no-repeat" />
+      <img src={sideImg} alt="side-img" className="hidden sm:block h-screen w-1/2 object-cover bg-no-repeat" />
 
       </>
     )}

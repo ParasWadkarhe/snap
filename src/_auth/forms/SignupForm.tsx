888e52eb68b85b6bb008c2b6bad11a10f01184be
ParasWadkarhe <paraswadkar11@gmail.com>
2025-01-05 13:33:10 +0530

    logo

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -51,7 +51,7 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-col flex-center">
-        <img src="/assets/images/logo.svg" alt="logo" />
+        <img src="/assets/images/logo.png" alt="logo" />
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
           To use snapgram, please enter your details
